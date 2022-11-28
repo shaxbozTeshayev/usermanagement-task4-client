0ec3600f769ea25716546843111ebce1903fd488
Shakhboz Teshaev <shaxbozteshayev7333@gmail.com>
2022-11-29 00:26:46 +0500

    fixed

--- src/config.js
+++ src/config.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: process.env.REACT_APP_URL,
-  baseURL: "https://usermanagement-task-4.herokuapp.com/api/",
+  baseURL: "https://internship-task-4.herokuapp.com/api/",
   // headers: { Authorization: "Bearer " + token }
   // headers: {
   //     'x-auth-token': token
   // }
 });
+
+// https://usermanagement-task-4.herokuapp.com
